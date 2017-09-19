b7d37662fdcf3c24b67341912c722933f33c7758
Thiago Brito <thiagosbrito@thiagosbrito-2.local>
2017-09-19 15:14:07 -0300

    changes at gulpfile

--- heroku-web.js
+++ heroku-web.js
@@ -4,7 +4,11 @@ var app = express();
 var morgan = require('morgan');
 var logger = morgan('combined');
 var fs = require('fs');
+var proxy = require('http-proxy-middleware');
+
 var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
+
 app.use(morgan('combinedstream': accessLogStream}));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.use('/prices', proxy({target: 'http://www.pioneerinter.com/lista-de-precos/get?q=', changeOrigin: true}));
 app.listen(process.env.PORT || 5000);
