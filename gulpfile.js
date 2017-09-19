290019dc3915cc1534278b1355e8dba1ee953e6a
Thiago Brito <thiagosbrito@thiagosbrito-2.local>
2017-09-19 15:20:18 -0300

    changes at gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,9 @@ gulp.task('serve', gulp.series('inject', 'watch', 'browsersync'));
 gulp.task('serve:dist', gulp.series('default', 'browsersync:dist'));
 gulp.task('default', gulp.series('clean', 'build'));
 gulp.task('watch', watch);
-gulp.task('heroku:production', gulp.series('build'));
+gulp.task('heroku:production', function () {
+  console.log('Heroku is in Prod Mode');
+});
 
 function reloadBrowserSync(cb) {
   browserSync.reload();
