d214380a5f2e2c132652544671bfcecba4559b30
Thiago Brito <thiagosbrito@thiagosbrito-2.local>
2017-09-19 16:24:18 -0300

    Adjust on node server

--- conf/browsersync-dist.conf.js
+++ conf/browsersync-dist.conf.js
@@ -1,11 +1,19 @@
 const conf = require('./gulp.conf');
+var proxy = require('http-proxy-middleware');
+
+var jsonPlaceholderProxy = proxy('/prices', {
+  target: 'http://www.pioneerinter.com/lista-de-precos/get?q=',
+  changeOrigin: true,             // for vhosted sites, changes host header to match to target's host
+  logLevel: 'debug'
+});
 
 module.exports = function () {
   return {
     server: {
       baseDir: [
         conf.paths.dist
-      ]
+      ],
+      middleware: [jsonPlaceholderProxy],
     },
     open: false
   };
--- heroku-web.js
+++ heroku-web.js
@@ -8,7 +8,7 @@ var proxy = require('http-proxy-middleware');
 
 var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
 
-app.use(morgan('combinedstream': accessLogStream}));
+app.use(morgan('combinedstream',accessLogStream));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.use('/prices', proxy({target: 'http://www.pioneerinter.com/lista-de-precos/get?q=', changeOrigin: true}));
 app.listen(process.env.PORT || 5000);
