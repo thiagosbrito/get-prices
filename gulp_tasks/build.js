5d6a9c1c4a392a062e58751f721f4b74c1af63ad
Thiago Brito <thiagosbrito@thiagosbrito-2.local>
2017-09-21 22:38:00 -0300

    Changes at build profile

--- gulp_tasks/build.js
+++ gulp_tasks/build.js
@@ -30,7 +30,6 @@ function build() {
 
   return gulp.src(conf.path.tmp('/index.html'))
     .pipe(inject(partialsInjectFile, partialsInjectOptions))
-    .pipe($.replace('../bower_components/font-awesome/fonts', '../fonts'))
     .pipe(useref({}, lazypipe().pipe(sourcemaps.init, {loadMaps: true})))
     .pipe(jsFilter)
     .pipe(ngAnnotate())
